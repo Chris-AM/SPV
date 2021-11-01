e6da32f06cfde1a369140a64074f9ce119e0a09b
adamantiumcocks <navarro.zefito@gmail.com>
2021-11-01 18:34:11 -0300

    se genera ruta productos en index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,5 +10,8 @@ const port = process.env.PORT;
 
 //cors permite comparticion de recursos de origen cruzado 
 app.use(cors());
+
+//Rutas("ruta", "archivo de ruta")
+ app.use("/api/productos", require("./routes/productos.routes"));
 //se inicia servidor
 app.listen(port, ()=>{console.log("servidor en puerto", port)});
