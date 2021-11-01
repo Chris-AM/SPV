73614350e5cd96de5c9a7d0465796f1e2170cb9d
adamantiumcocks <navarro.zefito@gmail.com>
2021-11-01 16:29:21 -0300

    se levanta y se inicia servidor express, se permite cors y se injecta puerto desde dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,12 @@
-console.log("hola backend");
-console.log("chao backend!");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(cors());
+//se inicia servidor
+app.listen(port, ()=>{console.log("servidor en puerto", port)});
