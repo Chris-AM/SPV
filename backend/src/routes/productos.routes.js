50a29808e6b882aadde7ae8357187803c0945d51
adamantiumcocks <navarro.zefito@gmail.com>
2021-11-01 18:42:12 -0300

    se crea la ruta para ser usada en express

---
+++ backend/src/routes/productos.routes.js
@@ -0,0 +1,9 @@
+//aqui se crea la ruta para ser usada por express
+const{Router} = require("express");
+
+
+//se debe instanciar la ruta para ser usada
+const router = Router(); 
+
+//se debe exportar la ruta para ser usada 
+module.exports = router; 
