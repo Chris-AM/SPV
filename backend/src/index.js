313c8d2c0b8c1439e31563b4d7484b6639d39f13
chris.am <c.aranguizm@outlook.com>
2021-11-02 17:07:26 -0300

    se inyecta configuracion de base de datos

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const {dbConfig} = require('./config/db.config')
 
 //app levanta un servidor express
 const app = express();
 //port se declara puerto 
 const port = process.env.PORT;
+dbConfig();
 
 //cors permite comparticion de recursos de origen cruzado 
 app.use(cors());
