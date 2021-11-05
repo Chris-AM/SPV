1d78c5af6dc6bb05cc40d230b9336a5ef312b0e0
chris.am <c.aranguizm@outlook.com>
2021-11-04 22:36:10 -0300

    creacion de departamentos funcional

--- backend/src/controllers/departamento.controller.js
+++ backend/src/controllers/departamento.controller.js
@@ -1,32 +1,21 @@
 const { request, response } = require("express");
 const { sequelize } = require('../config/db.config');
-const initModels = require('../models/init-models').initModels;
-const models = initModels(sequelize);
+const Departamento = require("../models/departamento.model");
+//const initModels = require('../models/init-models').initModels;
+//const models = initModels(sequelize);
 
 const crearDepartamento = async (req = request, res = response) => {
-    const body = req.body;
-    console.log('body -->', body)
-    if(body === undefined){
-        
-    }
-    try {
-        const departamento = {
-            NOMBRE_DEPARTAMENTO: req.body.NOMBRE_DEPARTAMENTO,
-            DETALLE_DEPARTAMENTO: req.body.DETALLE_DEPARTAMENTO
-        }
-        const deptoBD = await models.DEPARTAMENTO.create(departamento);
-        console.log('deptoBD', deptoBD )
-        res.json({
-            ok: true,
-            departamento: deptoBD
-        });
-    } catch (error) {
-        console.log('Error --->', error);
-        res.status(500).json({
-            ok:false,
-            msg: 'Internal server error. Check logs'
-        })
-    }
+    const nombre  = req.body.NOMBRE_DEPARTAMENTO;
+    const detalle = req.body.DETALLE_DEPARTAMENTO;
+    console.log(nombre, detalle);
+    const departamento = await Departamento.create({
+        NOMBRE_DEPARTAMENTO: nombre,
+        DETALLE_DEPARTAMENTO: detalle
+    });
+    res.json({
+        message: "Departamento creado",
+        data: departamento
+    });
 }
 
 module.exports = {
