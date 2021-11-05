c28029e09ffad590e25509e3fb5a6152d06d0317
chris.am <c.aranguizm@outlook.com>
2021-11-05 01:56:04 -0300

    se genera crud para departamento y su ruta para gestionarlo

--- backend/src/controllers/departamento.controller.js
+++ backend/src/controllers/departamento.controller.js
@@ -1,23 +1,159 @@
 const { request, response } = require("express");
 const { sequelize } = require('../config/db.config');
 const Departamento = require("../models/departamento.model");
-//const initModels = require('../models/init-models').initModels;
-//const models = initModels(sequelize);
 
 const crearDepartamento = async (req = request, res = response) => {
-    const nombre  = req.body.NOMBRE_DEPARTAMENTO;
+    const nombre = req.body.NOMBRE_DEPARTAMENTO;
     const detalle = req.body.DETALLE_DEPARTAMENTO;
-    console.log(nombre, detalle);
-    const departamento = await Departamento.create({
-        NOMBRE_DEPARTAMENTO: nombre,
-        DETALLE_DEPARTAMENTO: detalle
-    });
-    res.json({
-        message: "Departamento creado",
-        data: departamento
-    });
+    try {
+        const existeDepto = await Departamento.findOne({
+            where: {
+                NOMBRE_DEPARTAMENTO: nombre
+            }
+        });
+        if (existeDepto) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El departamento ya existe'
+            });
+        }
+        const departamento = new Departamento({
+            NOMBRE_DEPARTAMENTO: nombre,
+            DETALLE_DEPARTAMENTO: detalle
+        });
+        console.log('depto =>', departamento);
+        await departamento.save();
+        res.json({
+            ok: true,
+            msg: 'Departamento creado correctamente'
+        });
+    } catch (error) {
+        console.log('Error ===>', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Interno de Servidor. Checkar Logs'
+        });
+    }
+}
+const obtenerDepartamentos = async (req = request, res = response) => {
+    try {
+        const departamentos = await Departamento.findAll();
+        res.json({
+            ok: true,
+            departamentos
+        });
+    } catch (error) {
+        console.log('Error ===>', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Interno de Servidor. Checkar Logs'
+        });
+    }
+}
+
+const obtenerDepartamentoPorId = async (req = request, res = response) => {
+    const id = req.params.id;
+    try {
+        const departamento = await Departamento.findOne({
+            where: {
+                ID_DEPARTAMENTO: id
+            }
+        });
+        if (!departamento) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El departamento no existe'
+            });
+        }
+        res.json({
+            ok: true,
+            departamento
+        });
+    } catch (error) {
+        console.log('Error ===>', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Interno de Servidor. Checkar Logs'
+        });
+    }
+}
+const actualizarDepartamento = async (req = request, res = response) => {
+    const id = req.params.id;
+    try {
+        const deptoEnBd = await Departamento.findByPk(id);
+        if (!deptoEnBd) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El departamento no existe'
+            });
+        }
+        const nombre = req.body.NOMBRE_DEPARTAMENTO;
+        const detalle = req.body.DETALLE_DEPARTAMENTO;
+        if (deptoEnBd.NOMBRE_DEPARTAMENTO !== nombre) {
+            const existeDepto = await Departamento.findOne({
+                where: {
+                    NOMBRE_DEPARTAMENTO: nombre
+                }
+            });
+            if (existeDepto) {
+                return res.status(401).json({
+                    ok: false,
+                    msg: 'El departamento ya existe'
+                });
+            }
+        }
+        await Departamento.update({
+            NOMBRE_DEPARTAMENTO: nombre,
+            DETALLE_DEPARTAMENTO: detalle
+        }, {
+            where: {
+                ID_DEPARTAMENTO: id
+            }
+        });
+        res.json({
+            ok: true,
+            msg: 'Departamento actualizado correctamente'
+        });
+    } catch (error) {
+        console.log('Error ===>', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Interno de Servidor. Checkar Logs'
+        });
+    }
+}
+const eliminarDepartamento = async (req = request, res = response) => {
+    const id = req.params.id;
+    try {
+        const deptoEnBd = await Departamento.findByPk(id);
+        if (!deptoEnBd) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El departamento no existe'
+            });
+        }
+        await Departamento.destroy({
+            where: {
+                ID_DEPARTAMENTO: id
+            }
+        });
+        res.json({
+            ok: true,
+            msg: 'Departamento eliminado correctamente'
+        });
+    } catch (error) {
+        console.log('Error ===>', error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error Interno de Servidor. Checkar Logs'
+        });
+    }
 }
 
 module.exports = {
-    crearDepartamento
+    crearDepartamento,
+    obtenerDepartamentos,
+    obtenerDepartamentoPorId,
+    actualizarDepartamento,
+    eliminarDepartamento
 }
--- backend/src/routes/departamento.routes.js
+++ backend/src/routes/departamento.routes.js
@@ -1,12 +1,22 @@
 // /api/departamentos
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { crearDepartamento } = require('../controllers/departamento.controller');
+const { crearDepartamento,
+    obtenerDepartamentos,
+    obtenerDepartamentoPorId,
+    actualizarDepartamento,
+    eliminarDepartamento } = require('../controllers/departamento.controller');
 const router = Router();
 
-router.get('/');
+router.get('/', obtenerDepartamentos);
+router.get('/:id', obtenerDepartamentoPorId);
 router.post('/', [
     check('NOMBRE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty(),
     check('DETALLE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty()
 ], crearDepartamento);
+router.put('/:id', [
+    check('NOMBRE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty(),
+    check('DETALLE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty()
+], actualizarDepartamento);
+router.delete('/:id', eliminarDepartamento);
 module.exports = router;
