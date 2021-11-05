a6b497b030216237644e62d1b63b1314810d1e94
chris.am <c.aranguizm@outlook.com>
2021-11-04 22:40:12 -0300

    creacion de departamentos funcional

--- backend/src/routes/departamento.routes.js
+++ backend/src/routes/departamento.routes.js
@@ -1,8 +1,12 @@
 // /api/departamentos
-const {Router} = require("express");
-const {crearDepartamento} = require('../controllers/departamento.controller');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { crearDepartamento } = require('../controllers/departamento.controller');
 const router = Router();
 
 router.get('/');
-router.post('/', crearDepartamento);
+router.post('/', [
+    check('NOMBRE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty(),
+    check('DETALLE_DEPARTAMENTO', 'El codigo de barras es obligatorio').not().isEmpty()
+], crearDepartamento);
 module.exports = router;
